e174035cbf1758b7a2eafa741ed531dfeb14a408
Jéssica Hickembick <j.hickembick@gmail.com>
2023-10-10 09:52:50 -0300

    criaçao do codigo da SRC/TaskListm

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,1 +1,21 @@
 //* Este componente será responsável por exibir a lista de tarefas. Ele pode receber uma lista de tarefas como propriedade e mapeá-las para criar uma lista de TaskItem
+
+import React from 'react';
+import TaskItem from './TaskItem';
+
+function TaskList({ tasks, onComplete, onDelete }) {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onComplete={onComplete}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
