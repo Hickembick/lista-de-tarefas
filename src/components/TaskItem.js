d4784df9eacb2d3b081ecec39b5c5757ffa8d322
Jéssica Hickembick <j.hickembick@gmail.com>
2023-10-10 09:51:04 -0300

    criaçao do codigo da SRC/TaskForm

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,1 +1,15 @@
 //*Este componente representa uma tarefa individual. Ele pode exibir o texto da tarefa, seu estado (concluída ou não) e botões para editar e marcar como concluída.
+
+import React from 'react';
+
+function TaskItem({ task, onComplete, onDelete }) {
+  return (
+    <div>
+      <span>{task.text}</span>
+      <button onClick={() => onComplete(task.id)}>Concluído</button>
+      <button onClick={() => onDelete(task.id)}>Excluir</button>
+    </div>
+  );
+}
+
+export default TaskItem;
